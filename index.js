9957ac139fc12a4e26c58fb8fc92ebcbea143bd4
0V13TA <vincentpaulomorogbe6@gmail.com>
2025-05-02 17:10:35 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,263 @@
+"use strict";
+//#region Variables and Functions
+var _a;
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
+canvas.width = innerWidth * 0.8;
+canvas.height = innerHeight * 0.7;
+const center = { x: canvas.width / 2, y: canvas.height / 2 };
+const sizeOfHomes = { width: canvas.width * 0.4, height: canvas.height * 0.4 };
+const sizeOfTile = {
+    width: sizeOfHomes.width / 6,
+    height: (canvas.height * 0.2) / 3,
+};
+const leftTiles = [[], [], []];
+const rightTiles = [[], [], []];
+const topTiles = [[], [], []];
+const bottomTiles = [[], [], []];
+const specialTiles = [];
+const staringTiles = [];
+let noOfPlayer = 4;
+const get2randInt = () => [
+    Math.floor(Math.random() * 6 + 1),
+    Math.floor(Math.random() * 6 + 1),
+];
+let isPlaying = false;
+class Home {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.width = sizeOfHomes.width;
+        this.height = sizeOfHomes.height;
+        this.border = 20;
+        this.name = this.color === "green" && noOfPlayer === 3 ? "none" : color;
+        this.discX = 50;
+        this.discY = 60;
+        this.startingTile = null;
+        this.discs = [
+            new Disc(this.x + this.discX, this.y + this.discY, this.color, this.color),
+            new Disc(this.x + this.width - this.discX, this.y + this.discY, this.color, this.color),
+            new Disc(this.x + this.discX, this.y + this.height - this.discY, this.color, this.color),
+            new Disc(this.x + this.width - this.discX, this.y + this.height - this.discY, this.color, this.color),
+        ];
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.x + this.border / 2, this.y + this.border / 2, this.width - this.border, this.height - this.border);
+        ctx.fill();
+        ctx.closePath();
+        this.discs.forEach((disc) => disc.draw());
+    }
+}
+class Heaven {
+    constructor() {
+        this.width = canvas.width * 0.2;
+        this.height = canvas.height * 0.2;
+        this.x = center.x - this.width / 2;
+        this.y = center.y - this.height / 2;
+        this.color = "pink";
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+        ctx.fillStyle = "red";
+        ctx.font = "27px sans-serif";
+        ctx.fillText("LUDO", center.x - 37, center.y + 9);
+        ctx.closePath();
+    }
+}
+class Tile {
+    constructor(x, y, color, nextTile) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.height = sizeOfTile.height;
+        this.width = sizeOfTile.width;
+        this.nextTile = nextTile;
+        this.disc = null;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = "black";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+class Disc {
+    constructor(x, y, color, house) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.house = house;
+        this.isOutOfHouse = false;
+        this.radius = sizeOfTile.height / 2 - 2;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = "black";
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+//#endregion
+//#region Initializations
+for (let i = 0; i < 3; i++) {
+    let xCoord = 0, yCoord = sizeOfHomes.height;
+    let xCoord1 = sizeOfHomes.width + 3 * sizeOfTile.width, yCoord1 = sizeOfHomes.height;
+    for (let j = 0; j < 6; j++) {
+        leftTiles[i].push(new Tile(xCoord, yCoord + i * sizeOfTile.height, "white", null));
+        rightTiles[i].push(new Tile(xCoord1, yCoord1 + i * sizeOfTile.height, "white", null));
+        xCoord += sizeOfTile.width;
+        xCoord1 += sizeOfTile.width;
+    }
+}
+for (let i = 0; i < 3; i++) {
+    let xCoord = sizeOfHomes.width, yCoord = sizeOfHomes.height - sizeOfTile.height;
+    let xCoord1 = sizeOfHomes.width, yCoord1 = sizeOfHomes.height + 3 * sizeOfTile.height;
+    for (let j = 0; j < 6; j++) {
+        topTiles[i].push(new Tile(xCoord + i * sizeOfTile.width, yCoord, "white", null));
+        bottomTiles[i].push(new Tile(xCoord1 + i * sizeOfTile.width, yCoord1, "white", null));
+        yCoord -= sizeOfTile.height;
+        yCoord1 += sizeOfTile.height;
+    }
+}
+leftTiles.forEach((row, i) => {
+    row.forEach((tile, j) => {
+        if (j === 4 || j === 1) {
+            if (j === 1 && i === 0)
+                tile.color = "green";
+            if (j === 4 && i === 0)
+                bottomTiles[i][j].color = "yellow";
+            if (i === 2 && j === 4) {
+                rightTiles[i][j].color = "blue";
+                topTiles[i][j].color = "red";
+            }
+        }
+        if (i === 1) {
+            tile.color = "green";
+            rightTiles[i][j].color = "blue";
+            topTiles[i][j].color = "red";
+            bottomTiles[i][j].color = "yellow";
+            if (j === 0) {
+                tile.color = "white";
+                specialTiles.push(tile);
+            }
+            if (j === 5) {
+                rightTiles[i][j].color = "white";
+                topTiles[i][j].color = "white";
+                bottomTiles[i][j].color = "white";
+                specialTiles.push(rightTiles[i][j], topTiles[i][j], bottomTiles[i][j]);
+            }
+        }
+        if (i === 0) {
+            if (j === 5) {
+                tile.nextTile = topTiles[i][0];
+                topTiles[i][j].nextTile = topTiles[1][j];
+                rightTiles[i][j].nextTile = rightTiles[1][j];
+                bottomTiles[i][j].nextTile = bottomTiles[i][j - 1];
+            }
+            else {
+                tile.nextTile = leftTiles[i][j + 1];
+                topTiles[i][j].nextTile = topTiles[i][j + 1];
+                rightTiles[i][j].nextTile = rightTiles[i][j + 1];
+                bottomTiles[i][j].nextTile =
+                    j === 0 ? leftTiles[2][5] : bottomTiles[i][j - 1];
+            }
+            if (j === 1)
+                staringTiles.push(tile);
+            if (j === 4)
+                staringTiles.push(bottomTiles[i][j]);
+        }
+        if (i === 1) {
+            leftTiles[i][0].nextTile = leftTiles[0][0];
+            topTiles[i][5].nextTile = topTiles[2][5];
+            rightTiles[i][5].nextTile = rightTiles[2][5];
+            bottomTiles[i][5].nextTile = bottomTiles[0][5];
+        }
+        if (i === 2) {
+            if (j === 0) {
+                tile.nextTile = leftTiles[1][0];
+                topTiles[i][j].nextTile = rightTiles[0][0];
+                rightTiles[i][j].nextTile = bottomTiles[2][0];
+                bottomTiles[i][j].nextTile = bottomTiles[i][j + 1];
+            }
+            else if (j === 5) {
+                tile.nextTile = leftTiles[i][j - 1];
+                topTiles[i][j].nextTile = topTiles[i][j - 1];
+                rightTiles[i][j].nextTile = rightTiles[i][j - 1];
+                bottomTiles[i][j].nextTile = bottomTiles[1][5];
+            }
+            else {
+                tile.nextTile = leftTiles[i][j - 1];
+                topTiles[i][j].nextTile = topTiles[i][j - 1];
+                rightTiles[i][j].nextTile = rightTiles[i][j - 1];
+                bottomTiles[i][j].nextTile = bottomTiles[i][j + 1];
+            }
+            if (j === 4)
+                staringTiles.push(topTiles[i][j], rightTiles[i][j]);
+        }
+        tile.draw();
+        rightTiles[i][j].draw();
+        topTiles[i][j].draw();
+        bottomTiles[i][j].draw();
+    });
+});
+// The Arrangement matters and should not be changed
+const HOMES = [
+    new Home(0, 0, "green"),
+    new Home(0, canvas.height - sizeOfHomes.height, "yellow"),
+    new Home(canvas.width - sizeOfHomes.width, 0, "red"),
+    new Home(canvas.width - sizeOfHomes.width, canvas.height - sizeOfHomes.height, "blue"),
+];
+HOMES.forEach((home) => home.draw());
+const heaven = new Heaven();
+heaven.draw();
+//#endregion
+//#region EventListners
+(_a = document.querySelector("button#roll")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    if (!isPlaying) {
+        const time = setInterval(() => {
+            var _a, _b;
+            const [rand, rand1] = get2randInt();
+            document
+                .querySelectorAll("li.selected")
+                .forEach((el) => el.classList.remove("selected"));
+            (_a = document
+                .querySelector(`ul.firstDie li#id${rand}`)) === null || _a === void 0 ? void 0 : _a.classList.add("selected");
+            (_b = document
+                .querySelector(`ul.secondDie li#id${rand1}`)) === null || _b === void 0 ? void 0 : _b.classList.add("selected");
+        }, 10);
+        const [rand, rand1] = get2randInt();
+        setTimeout(() => {
+            var _a, _b;
+            clearInterval(time);
+            document
+                .querySelectorAll("li.selected")
+                .forEach((el) => el.classList.remove("selected"));
+            (_a = document
+                .querySelector(`ul.firstDie li#id${rand}`)) === null || _a === void 0 ? void 0 : _a.classList.add("selected");
+            (_b = document
+                .querySelector(`ul.secondDie li#id${rand1}`)) === null || _b === void 0 ? void 0 : _b.classList.add("selected");
+        }, 500);
+        console.log(rand, rand1);
+    }
+});
+//#endregion
+console.log(staringTiles);
---
+++ index.ts
@@ -0,0 +1,375 @@
+//#region Variables and Functions
+
+const canvas = document.createElement("canvas");
+const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+document.body.appendChild(canvas);
+
+canvas.width = innerWidth * 0.8;
+canvas.height = innerHeight * 0.7;
+const center = { x: canvas.width / 2, y: canvas.height / 2 };
+const sizeOfHomes = { width: canvas.width * 0.4, height: canvas.height * 0.4 };
+const sizeOfTile = {
+  width: sizeOfHomes.width / 6,
+  height: (canvas.height * 0.2) / 3,
+};
+
+const leftTiles: [Tile[], Tile[], Tile[]] = [[], [], []];
+const rightTiles: [Tile[], Tile[], Tile[]] = [[], [], []];
+const topTiles: [Tile[], Tile[], Tile[]] = [[], [], []];
+const bottomTiles: [Tile[], Tile[], Tile[]] = [[], [], []];
+const specialTiles: Tile[] = [];
+const staringTiles: Tile[] = [];
+let noOfPlayer = 4;
+
+const get2randInt = (): [number, number] => [
+  Math.floor(Math.random() * 6 + 1),
+  Math.floor(Math.random() * 6 + 1),
+];
+let isPlaying = false;
+
+//#endregion
+
+//#region Classes
+
+interface Home {
+  x: number;
+  y: number;
+  color: string;
+  width: number;
+  height: number;
+  border: number;
+  name: string;
+  discX: number;
+  discY: number;
+  discs: Disc[];
+  startingTile: Tile | null;
+  draw(): void;
+}
+class Home {
+  constructor(x: number, y: number, color: string) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.width = sizeOfHomes.width;
+    this.height = sizeOfHomes.height;
+    this.border = 20;
+    this.name = this.color === "green" && noOfPlayer === 3 ? "none" : color;
+    this.discX = 50;
+    this.discY = 60;
+    this.startingTile = null;
+    this.discs = [
+      new Disc(
+        this.x + this.discX,
+        this.y + this.discY,
+        this.color,
+        this.color
+      ),
+      new Disc(
+        this.x + this.width - this.discX,
+        this.y + this.discY,
+        this.color,
+        this.color
+      ),
+      new Disc(
+        this.x + this.discX,
+        this.y + this.height - this.discY,
+        this.color,
+        this.color
+      ),
+      new Disc(
+        this.x + this.width - this.discX,
+        this.y + this.height - this.discY,
+        this.color,
+        this.color
+      ),
+    ];
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.fillRect(
+      this.x + this.border / 2,
+      this.y + this.border / 2,
+      this.width - this.border,
+      this.height - this.border
+    );
+    ctx.fill();
+    ctx.closePath();
+    this.discs.forEach((disc) => disc.draw());
+  }
+}
+
+interface Heaven {
+  x: number;
+  y: number;
+  color: string;
+  width: number;
+  height: number;
+  draw(): void;
+}
+class Heaven {
+  constructor() {
+    this.width = canvas.width * 0.2;
+    this.height = canvas.height * 0.2;
+    this.x = center.x - this.width / 2;
+    this.y = center.y - this.height / 2;
+    this.color = "pink";
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fill();
+    ctx.fillStyle = "red";
+    ctx.font = "27px sans-serif";
+    ctx.fillText("LUDO", center.x - 37, center.y + 9);
+    ctx.closePath();
+  }
+}
+
+interface Tile {
+  x: number;
+  y: number;
+  color: string;
+  width: number;
+  height: number;
+  nextTile: Tile | null;
+  disc: Disc | null;
+  draw(): void;
+}
+class Tile {
+  constructor(x: number, y: number, color: string, nextTile: Tile | null) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.height = sizeOfTile.height;
+    this.width = sizeOfTile.width;
+    this.nextTile = nextTile;
+    this.disc = null;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = "black";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+interface Disc {
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  house: string;
+  isOutOfHouse: boolean;
+  draw(): void;
+}
+class Disc {
+  constructor(x: number, y: number, color: string, house: string) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.house = house;
+    this.isOutOfHouse = false;
+    this.radius = sizeOfTile.height / 2 - 2;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = "black";
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+//#endregion
+
+//#region Initializations
+
+for (let i = 0; i < 3; i++) {
+  let xCoord = 0,
+    yCoord = sizeOfHomes.height;
+  let xCoord1 = sizeOfHomes.width + 3 * sizeOfTile.width,
+    yCoord1 = sizeOfHomes.height;
+  for (let j = 0; j < 6; j++) {
+    leftTiles[i].push(
+      new Tile(xCoord, yCoord + i * sizeOfTile.height, "white", null)
+    );
+    rightTiles[i].push(
+      new Tile(xCoord1, yCoord1 + i * sizeOfTile.height, "white", null)
+    );
+    xCoord += sizeOfTile.width;
+    xCoord1 += sizeOfTile.width;
+  }
+}
+
+for (let i = 0; i < 3; i++) {
+  let xCoord = sizeOfHomes.width,
+    yCoord = sizeOfHomes.height - sizeOfTile.height;
+  let xCoord1 = sizeOfHomes.width,
+    yCoord1 = sizeOfHomes.height + 3 * sizeOfTile.height;
+  for (let j = 0; j < 6; j++) {
+    topTiles[i].push(
+      new Tile(xCoord + i * sizeOfTile.width, yCoord, "white", null)
+    );
+    bottomTiles[i].push(
+      new Tile(xCoord1 + i * sizeOfTile.width, yCoord1, "white", null)
+    );
+    yCoord -= sizeOfTile.height;
+    yCoord1 += sizeOfTile.height;
+  }
+}
+
+leftTiles.forEach((row, i) => {
+  row.forEach((tile, j) => {
+    if (j === 4 || j === 1) {
+      if (j === 1 && i === 0) tile.color = "green";
+      if (j === 4 && i === 0) bottomTiles[i][j].color = "yellow";
+      if (i === 2 && j === 4) {
+        rightTiles[i][j].color = "blue";
+        topTiles[i][j].color = "red";
+      }
+    }
+
+    if (i === 1) {
+      tile.color = "green";
+      rightTiles[i][j].color = "blue";
+      topTiles[i][j].color = "red";
+      bottomTiles[i][j].color = "yellow";
+
+      if (j === 0) {
+        tile.color = "white";
+        specialTiles.push(tile);
+      }
+
+      if (j === 5) {
+        rightTiles[i][j].color = "white";
+        topTiles[i][j].color = "white";
+        bottomTiles[i][j].color = "white";
+        specialTiles.push(rightTiles[i][j], topTiles[i][j], bottomTiles[i][j]);
+      }
+    }
+
+    if (i === 0) {
+      if (j === 5) {
+        tile.nextTile = topTiles[i][0];
+        topTiles[i][j].nextTile = topTiles[1][j];
+        rightTiles[i][j].nextTile = rightTiles[1][j];
+        bottomTiles[i][j].nextTile = bottomTiles[i][j - 1];
+      } else {
+        tile.nextTile = leftTiles[i][j + 1];
+        topTiles[i][j].nextTile = topTiles[i][j + 1];
+        rightTiles[i][j].nextTile = rightTiles[i][j + 1];
+        bottomTiles[i][j].nextTile =
+          j === 0 ? leftTiles[2][5] : bottomTiles[i][j - 1];
+      }
+
+      if (j === 1) staringTiles.push(tile);
+      if (j === 4) staringTiles.push(bottomTiles[i][j]);
+    }
+
+    if (i === 1) {
+      leftTiles[i][0].nextTile = leftTiles[0][0];
+      topTiles[i][5].nextTile = topTiles[2][5];
+      rightTiles[i][5].nextTile = rightTiles[2][5];
+      bottomTiles[i][5].nextTile = bottomTiles[0][5];
+    }
+
+    if (i === 2) {
+      if (j === 0) {
+        tile.nextTile = leftTiles[1][0];
+        topTiles[i][j].nextTile = rightTiles[0][0];
+        rightTiles[i][j].nextTile = bottomTiles[2][0];
+        bottomTiles[i][j].nextTile = bottomTiles[i][j + 1];
+      } else if (j === 5) {
+        tile.nextTile = leftTiles[i][j - 1];
+        topTiles[i][j].nextTile = topTiles[i][j - 1];
+        rightTiles[i][j].nextTile = rightTiles[i][j - 1];
+        bottomTiles[i][j].nextTile = bottomTiles[1][5];
+      } else {
+        tile.nextTile = leftTiles[i][j - 1];
+        topTiles[i][j].nextTile = topTiles[i][j - 1];
+        rightTiles[i][j].nextTile = rightTiles[i][j - 1];
+        bottomTiles[i][j].nextTile = bottomTiles[i][j + 1];
+      }
+
+      if (j === 4) staringTiles.push(topTiles[i][j], rightTiles[i][j]);
+    }
+
+    tile.draw();
+    rightTiles[i][j].draw();
+    topTiles[i][j].draw();
+    bottomTiles[i][j].draw();
+  });
+});
+
+// The Arrangement matters and should not be changed
+const HOMES = [
+  new Home(0, 0, "green"),
+  new Home(0, canvas.height - sizeOfHomes.height, "yellow"),
+  new Home(canvas.width - sizeOfHomes.width, 0, "red"),
+  new Home(
+    canvas.width - sizeOfHomes.width,
+    canvas.height - sizeOfHomes.height,
+    "blue"
+  ),
+];
+HOMES.forEach((home) => home.draw());
+const heaven = new Heaven();
+
+heaven.draw();
+
+//#endregion
+
+//#region EventListners
+
+document.querySelector("button#roll")?.addEventListener("click", () => {
+  if (!isPlaying) {
+    const time = setInterval(() => {
+      const [rand, rand1] = get2randInt();
+      document
+        .querySelectorAll("li.selected")
+        .forEach((el) => el.classList.remove("selected"));
+      document
+        .querySelector(`ul.firstDie li#id${rand}`)
+        ?.classList.add("selected");
+      document
+        .querySelector(`ul.secondDie li#id${rand1}`)
+        ?.classList.add("selected");
+    }, 10);
+
+    const [rand, rand1] = get2randInt();
+    setTimeout(() => {
+      clearInterval(time);
+      document
+        .querySelectorAll("li.selected")
+        .forEach((el) => el.classList.remove("selected"));
+      document
+        .querySelector(`ul.firstDie li#id${rand}`)
+        ?.classList.add("selected");
+      document
+        .querySelector(`ul.secondDie li#id${rand1}`)
+        ?.classList.add("selected");
+    }, 500);
+    console.log(rand, rand1);
+  }
+});
+
+//#endregion
+
+console.log(staringTiles);
